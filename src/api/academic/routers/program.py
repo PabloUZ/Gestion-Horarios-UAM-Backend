f37ac4cb967b3387e7bb99071e5dc1cad609d098
JulianV122 <julivelo69@gmail.com>
2024-05-10 22:03:08 -0500

    Creacion de routers de academic

---
+++ src/api/academic/routers/program.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from src.api.academic.schemas.program import Program
+from src.api.academic.repositories.program import ProgramRepository
+from fastapi import Body, Path
+from fastapi.responses import JSONResponse
+from typing import List
+from fastapi.encoders import jsonable_encoder
+from src.api.config.database import SessionLocal
+from src.api.academic.models.program import Program as ProgramModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.params import Query
+from fastapi import status
+router = APIRouter(prefix="/api/v1/program", tags=["Program"])
+
+@router.get("/", response_model=List[Program], description="Get all programs")
+def get_programs(offset:int = Query(default=None,min = 0), limit:int = Query(default=None,min = 1)) -> List[Program]:
+    db = SessionLocal()
+    programs = ProgramRepository(db).get_programs(offset,limit)
+    return JSONResponse(content=jsonable_encoder(programs), status_code=status.HTTP_200_OK)
+
+@router.get("/{program_id}", response_model=Program, description="Get a program by id")
+def get_program_by_id(program_id: int = Path(ge=1)) -> Program:
+    db = SessionLocal()
+    program = ProgramRepository(db).get_program(program_id)
+    if not program:
+        return JSONResponse(content={"message": "Program not found", "data":None}, status_code=status.HTTP_404_NOT_FOUND)
+    return JSONResponse(content=jsonable_encoder(program), status_code=status.HTTP_200_OK)
+
+@router.post("/", response_model=Program, description="Create a new program")
+def create_program(program: Program = Body()) -> dict:
+    db = SessionLocal()
+    new_program = ProgramRepository(db).create_program(program)
+    return JSONResponse(content={"message":"Program created succesfully", "data": jsonable_encoder(new_program)}, status_code=status.HTTP_201_CREATED)
+
+@router.put("/{program_id}", response_model=Program, description="Update a program by id")
+def update_program(program_id: int = Path(ge=1), program: Program = Body()) -> dict:
+    db = SessionLocal()
+    upd_program = ProgramRepository(db).get_program(program_id)
+    if not upd_program:
+        return JSONResponse(content={"message": "Program not found", "data":None}, status_code=status.HTTP_404_NOT_FOUND)
+    
+    upd_program = ProgramRepository(db).update_program(program_id, program)
+    return JSONResponse(content={"message":"Program updated succesfully", "data": jsonable_encoder(upd_program)}, status_code=status.HTTP_200_OK)
+
+@router.delete("/{program_id}",response_model=dict, description="Delete a program by id")
+def delete_program(program_id: int = Path(ge=1)) -> dict:
+    db = SessionLocal()
+    del_program = ProgramRepository(db).get_program(program_id)
+    if not del_program:
+        return JSONResponse(content={"message": "Program not found", "data":None}, status_code=status.HTTP_404_NOT_FOUND)
+    
+    ProgramRepository(db).delete_program(program_id)
+    return JSONResponse(content={"message":"Program deleted succesfully", "data":None}, status_code=status.HTTP_200_OK)
---
+++ src/api/academic/routers/study_plan.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter
+from src.api.academic.schemas.study_plan import StudyPlan
+from src.api.academic.repositories.study_plan import StudyPlanRepository
+from fastapi import Body, Path
+from fastapi.responses import JSONResponse
+from typing import List
+from fastapi.encoders import jsonable_encoder
+from src.api.config.database import SessionLocal
+from src.api.academic.models.study_plan import StudyPlan as StudyPlanModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.params import Query
+from fastapi import status
+
+router = APIRouter(prefix="/api/v1/study_plan", tags=["Study Plan"])
+
+@router.get("/", response_model=List[StudyPlan], description="Get all study plans")
+def get_study_plans(offset:int = Query(default=None,min = 0), limit:int = Query(default=None,min = 1)) -> List[StudyPlan]:
+    db = SessionLocal()
+    study_plans = StudyPlanRepository(db).get_study_plans(offset,limit)
+    return JSONResponse(content=jsonable_encoder(study_plans), status_code=status.HTTP_200_OK)
+
+@router.get("/{study_plan_id}", response_model=StudyPlan, description="Get a study plan by id")
+def get_study_plan_by_id(study_plan_id: int = Path(ge=1)) -> StudyPlan:
+    db = SessionLocal()
+    study_plan = StudyPlanRepository(db).get_study_plan(study_plan_id)
+    if not study_plan:
+        return JSONResponse(content={"message": "Study Plan not found", "data":None}, status_code=status.HTTP_404_NOT_FOUND)
+    return JSONResponse(content=jsonable_encoder(study_plan), status_code=status.HTTP_200_OK)
+
+@router.post("/", response_model=StudyPlan, description="Create a new study plan")
+def create_study_plan(study_plan: StudyPlan = Body()) -> dict:
+    db = SessionLocal()
+    new_study_plan = StudyPlanRepository(db).create_study_plan(study_plan)
+    return JSONResponse(content={"message":"Study Plan created succesfully", "data": jsonable_encoder(new_study_plan)}, status_code=status.HTTP_201_CREATED)
+
+@router.put("/{study_plan_id}", response_model=StudyPlan, description="Update a study plan by id")
+def update_study_plan(study_plan_id: int = Path(ge=1), study_plan: StudyPlan = Body()) -> dict:
+    db = SessionLocal()
+    upd_study_plan = StudyPlanRepository(db).get_study_plan(study_plan_id)
+    if not upd_study_plan:
+        return JSONResponse(content={"message": "Study Plan not found", "data":None}, status_code=status.HTTP_404_NOT_FOUND)
+    
+    upd_study_plan = StudyPlanRepository(db).update_study_plan(study_plan_id, study_plan)
+    return JSONResponse(content={"message":"Study Plan updated succesfully", "data": jsonable_encoder(upd_study_plan)}, status_code=status.HTTP_200_OK)
+
+@router.delete("/{study_plan_id}",response_model=dict, description="Delete a study plan by id")
+def delete_study_plan(study_plan_id: int = Path(ge=1)) -> dict:
+    db = SessionLocal()
+    del_study_plan = StudyPlanRepository(db).get_study_plan(study_plan_id)
+    if not del_study_plan:
+        return JSONResponse(content={"message": "Study Plan not found", "data":None}, status_code=status.HTTP_404_NOT_FOUND)
+    
+    StudyPlanRepository(db).delete_study_plan(study_plan_id)
+    return JSONResponse(content={"message":"Study Plan deleted succesfully", "data":None}, status_code=status.HTTP_200_OK)
